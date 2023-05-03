e8756e10e0188da33618d4ec17177f618f94278a
Marc Esteve Garcia <41911219+MarcEsteve@users.noreply.github.com>
2023-05-03 17:26:34 +0200

    más sobre PHP

--- segundo.php
+++ segundo.php
@@ -1,14 +1,17 @@
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Segundo PHP</title>
 </head>
+
 <body>
     <?php
-    //Código PHP
-    echo "Hola segundo";
+        //Código PHP
+        echo "Hola segundo";
     ?>
 </body>
+
 </html>
