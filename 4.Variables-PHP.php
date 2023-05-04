e08f0382034446afc5e39856f15edf69a8f0e966
Marc Esteve Garcia <41911219+MarcEsteve@users.noreply.github.com>
2023-05-04 15:38:54 +0200

    Update 4.Variables-PHP.php

--- 4.Variables-PHP.php
+++ 4.Variables-PHP.php
@@ -2,7 +2,8 @@
 
 // NOTAS:
 # Los nombres de las variables son sensibles a mayusculas y minisculas "Case-sensitive"
-# Los nombres de variables no pueden llevar espacios, empezar por numeros o llevar caracteres especiales como puntos o incluso la letra ñ
+# Los nombres de variables no pueden llevar espacios, empezar por numeros o llevar caracteres especiales como puntos o incluso la letra ñ o ç
+#Todas las variables empiezan con $
 
 // TIPOS de datos:
 # String: Cadena de texto
@@ -18,9 +19,17 @@
 
 // String
 $nombre = "Marc Esteve Garcia";
+/* 
+$texto ='Texto entre comillas';
+Comentarios largos de saltos de linea
+*/
+$numText = "25"; //Esto es un texto
 
-// Entero
+// Integer o Entero
 $numero = 13;
+
+// Double o Decimal
+$decimal = 0.16;
 
 // Boleano
 $verdadero_falso = true;
@@ -32,12 +41,16 @@ En las comillas dobles podemos llamar variables, mientras que en las simples no.
 Sin embargo usar comillas dobles en ciertas situaciones puede traernos problemas de seguridad.
 */
 
-echo 'Mi nombre es ' . $nombre;
-echo '<br>';
+echo 'Mi nombre es' . $nombre . '<br>';
+echo "Mi nombre es $nombre <br>";
 
-echo "Mi nombre es $nombre";
-
-// Para conocer el tipo de dato de una variable utilizamos la función gettype()
-echo gettype($nombre);
+/* 
+1ª Función gettype()
+Para conocer el tipo de dato de una variable utilizamos la función gettype()
+*/
+echo 'El tipo de dato de la variable $nombre es de tipo: ' . gettype($nombre);
+echo '<br>El tipo de dato de la variable $numero es de tipo: ' .gettype($numero);
+echo '<br>El tipo de dato de la variable $decimal es de tipo: ' .gettype($decimal);
+echo '<br>El tipo de dato de la variable $verdadero_falso es de tipo: ' .gettype($verdadero_falso);
 
 ?>
