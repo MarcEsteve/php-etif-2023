a8241b305f45fc5b38ef116541508e9f858bf064
Marc Esteve Garcia <41911219+MarcEsteve@users.noreply.github.com>
2023-05-04 16:31:36 +0200

    Update ejercicio-print.php

--- ejercicio-print.php
+++ ejercicio-print.php
@@ -4,7 +4,7 @@ $txt2 = "pr0j3ct.com";
 $x = 5; $y = 4;
 print "<h2>" . $txt1 . "</h2>";
 print "Estudia PHP con " . $txt2 . "<br>";
-$seejecuta = print $x + $y; //Le asigna un 1 a $variable1 porque tiene un return de un 1
+$variable1 = print $x + $y; //Le asigna un 1 a $variable1 porque tiene un return de un 1
 // $variable2 = echo ($x + $y); //No se le puede asignar a ninguna variable porque no devuelve nada
-print $variable1;
+print "<br>" . $variable1; //Mostrará un 1
 ?>
