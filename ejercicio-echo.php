d162061f85ba63299b8977c251aa85913aba6433
Marc Esteve Garcia <41911219+MarcEsteve@users.noreply.github.com>
2023-05-04 16:28:20 +0200

    más sobre PHP

---
+++ ejercicio-echo.php
@@ -0,0 +1,12 @@
+<?php
+
+echo "<h2> ¡PHP es divertido! </h2>";
+
+echo "¡Hola mundo! <br>";
+
+echo "¡Estoy a punto de aprender PHP! <br>";
+
+echo "Esta", " cadena de texto ", "se ha", "
+creado ", " con multiples parámetros.";
+
+?>
