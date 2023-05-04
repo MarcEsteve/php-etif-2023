571f80e6c654d9971ae81175645f20ab3512b79a
Marc Esteve Garcia <41911219+MarcEsteve@users.noreply.github.com>
2023-05-04 17:28:31 +0200

    print y echo

--- ejercicio-print.php
+++ ejercicio-print.php
@@ -7,11 +7,14 @@ print "Estudia PHP con " . $txt2 . "<br>";
 $variable1 = print $x + $y; //Le asigna un 1 a $variable1 porque tiene un return de un 1
 // $variable2 = echo ($x + $y); //No se le puede asignar a ninguna variable porque no devuelve nada
 print "<br>" . $variable1 . "<br>"; //Mostrará un 1
-print '<a href="ejercicio-echo.php">ejercicio de echo()</a>';
+
+print '<a href="ejercicio-echo.php">ejercicio echo</a>';
+# Comillas simple ' ' deben tener comillas dobles dentro
+# Comillas dobles " " deben tener comillas simples dentro
 
 //Aqui acaba el código de PHP
 ?>
 
 <!-- Si finalizamos el código php podriamos integrar HTML -->
 <br>
-<a href="ejercicio-echo.php">ejercicio de echo()</a>
+<a href="ejercicio-echo.php">ejercicio echo</a>
