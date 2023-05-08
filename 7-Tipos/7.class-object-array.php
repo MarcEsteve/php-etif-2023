c54a53d7f30c4844f6ac31b8146751f5d7a3ece7
Marc Esteve Garcia <41911219+MarcEsteve@users.noreply.github.com>
2023-05-08 17:23:45 +0200

    más sobre class onject

--- 7-Tipos/7.class-object-array.php
+++ 7-Tipos/7.class-object-array.php
@@ -2,27 +2,30 @@
 
     class Persona {
         //Atributos
-        public $nombre = array();
-        public $apellido = array();
+        private $nombre = array();
+        private $apellido = array();
 
         //Métodos
-        public function guardar($nombre, $apellido){
+        public function setGuardar($nombre, $apellido){
             $this->nombre[] = $nombre;
             $this->apellido[] = $apellido;
         }
-        public function mostrar(){
-            for($i=0; $i<count($this->nombre); $i++){
-                $this->formato($this->nombre[$i], $this->apellido[$i]);
+        public function getMostrar(){
+            for($i=0; $i<count($this->apellido); $i++){
+                $this->formatear($this->nombre[$i], $this->apellido[$i]);
             }
         }
-        public function formato($nombre, $apellido){
-            echo 'Nombre: ' . $nombre . '| Apellido: ' . $apellido . '<br>';
+        public function formatear($nombre, $apellido){
+            echo 'Nombre: ' . $nombre . ' | Apellido: ' . $apellido . '<br>';
         }
     }
 
     $persona = new Persona();
-    $persona->guardar('Jose', 'Fernández');
-    $persona->guardar('Arnau', 'Garcia');
-    $persona->mostrar();
+    $persona->getMostrar();
+    $persona->setGuardar('Jose', 'Fernández');
+    $persona->setGuardar('Arnau', 'Garcia');
+    $persona->setGuardar('Pedro', 'Jiménez');
+    $persona->setGuardar('Marc ', 'Esteve');
+    $persona->getMostrar();
 
 ?>
--- 7-Tipos/classprofesor.php
+++ 7-Tipos/classprofesor.php
@@ -21,17 +21,22 @@
         }
     }
     
+    //Instanciaciones de objetos
     $p1 = new Profesor();
-    //echo $p1->nombre; //No se puede porque los atributos son privados
-    $p1->setNombre("Juan");
-    echo $p1->getNombre(); // Imprime "Juan"
-
     $p2 = new Profesor();
-    $p2->setNombre("Pedro");
-    echo $p2->getNombre(); // Imprime "Pedro"
-
     $p3 = new Profesor();
+    //echo $p1->nombre; //No se puede porque los atributos son privados
+
+    //Damos valores con métodos SETTERS (función de INPUTS)
+    $p1->setNombre("Juan");
+    $p2->setNombre("Pedro");
     $p3->setNombre("Manolo");
-    echo $p3->getNombre(); // Imprime "Manolo"
+    $p1->setMateria("Inglés");
+    $p2->setMateria("Filosofia");
+    $p3->setMateria("Matemáticas");
+
+    //Ejercicio: Haz una frase con los tres profesores y su materia que imparten
+    echo "{$p1->getNombre()}, ". $p2->getNombre() . " y {$p3->getNombre()} son tres profesores de las asignaturas {$p1->getMateria()}, {$p2->getMateria()} y {$p3->getMateria()} respectivamente.";
+    //Obtendremos valores con métodos GETTERS (Funciones OUTPUTS)
 
 ?>
