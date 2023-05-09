56a03c290a02dd4490637b8fbe2947fa510d6a99
Marc Esteve Garcia <41911219+MarcEsteve@users.noreply.github.com>
2023-05-09 16:19:01 +0200

    ejercicio herencia php

--- 7-Tipos/clase-animal-herencia.php
+++ 7-Tipos/clase-animal-herencia.php
@@ -62,7 +62,4 @@ $vaca->eat();
 $gato->maullar();
 $perro->ladrar();
 $vaca->mugir();
-
-//Ejercicio: Crea de la misma estructura, la superclase Vehiculo, y las subclases Coche, Avion y Barco. Vehiculo tendrá al menos 1 atributo y 2 metodos específicos. Después cada sbuclase Coche, Avion y Barco tendrán al menos 1 atributo y 1 método específico
-
 ?>
---
+++ 7-Tipos/ejercicio-herencia.php
@@ -0,0 +1,5 @@
+<?php
+
+//Ejercicio: Crea de la misma estructura que clase-animal-herencia.php, la superclase Vehiculo, y las subclases Coche, Avion y Barco. Vehiculo tendrá al menos 1 atributo y 2 metodos específicos. Después cada sbuclase Coche, Avion y Barco tendrán al menos 1 atributo y 1 método específico
+
+?>
