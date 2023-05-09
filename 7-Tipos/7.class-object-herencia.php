e5cb5303d06ee46bbea6f6923402a9647b90944e
Marc Esteve Garcia <41911219+MarcEsteve@users.noreply.github.com>
2023-05-09 15:38:30 +0200

    Update 7.class-object-herencia.php

--- 7-Tipos/7.class-object-herencia.php
+++ 7-Tipos/7.class-object-herencia.php
@@ -1,7 +1,7 @@
 <?php
 
 //HERENCIA
-//Superclase
+//Superclase "Foo"
 class Foo {
     public function printItem($string) {
         echo 'Foo: ' . $string . PHP_EOL;
@@ -10,21 +10,22 @@ class Foo {
         echo 'PHP is great.' . PHP_EOL;
     }
 }
-
-//Subclase
-class bar extends Foo {
+//Subclase "bar"
+class Bar extends Foo {
+    //Este método printItem($string) también se hereda pero hay "sobreescritura"
     public function printItem($string) {
         echo 'Bar: ' . $string . PHP_EOL;
     }
+    //El método printPHP() se hereda
 }
 
-$foo = new Foo();
+$foo = new Foo(); //Instanciación de la clase para crear objetos
 $bar = new Bar();
 
-$foo->printItem('baz'); // Salida: 'Foo: baz'
-$foo->printPHP();       // Salida: 'PHP is great' 
+$foo->printItem('baz'); // Salida: 'Foo: baz' es método de class Foo()
+$foo->printPHP();       // Salida: 'PHP is great' es método de class Foo()
 
-$bar->printItem('baz'); // Salida: 'Bar: baz'
-$bar->printPHP();       // Salida: 'PHP is great'
+$bar->printItem('baz'); // Salida: 'Bar: baz' es método de class Bar()
+$bar->printPHP();       // Salida: 'PHP is great' es método heredado de la class Foo()
 
 ?>
