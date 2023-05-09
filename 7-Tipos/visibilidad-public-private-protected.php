92ac1bf73753f612277c92ac4c0649959a5f1b2a
Marc Esteve Garcia <41911219+MarcEsteve@users.noreply.github.com>
2023-05-09 15:21:08 +0200

    más ejemplos HERENCIA y VISIBILIDAD en PHP

--- 7-Tipos/visibilidad-public-private-protected.php
+++ 7-Tipos/visibilidad-public-private-protected.php
@@ -1,44 +1,15 @@
 <?php
 
-//HERENCIA
-class Foo {
-    public function printItem($string) {
-        echo 'Foo: ' . $string . PHP_EOL;
-    }
-
-    public function printPHP()
-    {
-        echo 'PHP is great.' . PHP_EOL;
-    }
-}
-
-class bar extends Foo
-{
-    public function printItem($string)
-    {
-        echo 'Bar: ' . $string . PHP_EOL;
-    }
-}
-
-$foo = new Foo();
-$bar = new Bar();
-$foo->printItem('baz'); // Salida: 'Foo: baz'
-$foo->printPHP();       // Salida: 'PHP is great' 
-$bar->printItem('baz'); // Salida: 'Bar: baz'
-$bar->printPHP();       // Salida: 'PHP is great'
-
 //VISIBILIDAD
 /**
  * Definición de MyClass
  */
-class MyClass
-{
+class MyClass {
     public $public = 'Public';
     protected $protected = 'Protected';
     private $private = 'Private';
 
-    function printHello()
-    {
+    function printHello(){
         echo $this->public;
         echo $this->protected;
         echo $this->private;
@@ -55,14 +26,12 @@ $obj->printHello();   // Muestra Public, Protected y Private
 /**
  * Definición de MyClass2
  */
-class MyClass2 extends MyClass
-{
+class MyClass2 extends MyClass {
     // Se pueden redeclarar las propiedades pública y protegida, pero no la privada
     public $public = 'Public2';
     protected $protected = 'Protected2';
 
-    function printHello()
-    {
+    function printHello(){
         echo $this->public;
         echo $this->protected;
         //echo $this->private;
@@ -74,3 +43,5 @@ echo $obj2->public;    // Funciona bien
 //echo $obj2->protected; // Error Fatal
 //echo $obj2->private;   // Undefined
 $obj2->printHello();   // Muestra Public2, Protected2, Undefined
+
+?>
