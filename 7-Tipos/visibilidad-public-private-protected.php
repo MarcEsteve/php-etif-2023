0bb2f86a358686728bf8d1a1c8753d5fa5b31321
Marc Esteve Garcia <41911219+MarcEsteve@users.noreply.github.com>
2023-05-09 15:57:22 +0200

    Update visibilidad-public-private-protected.php

--- 7-Tipos/visibilidad-public-private-protected.php
+++ 7-Tipos/visibilidad-public-private-protected.php
@@ -2,21 +2,22 @@
 
 //VISIBILIDAD
 /**
- * Definición de MyClass
+ * Definición de MyClass "Superclase"
  */
 class MyClass {
+    //3 atributos con modificadores de VISIBILIDAD
     public $public = 'Public';
     protected $protected = 'Protected';
     private $private = 'Private';
 
-    function printHello(){
+    public function printHello(){
         echo $this->public;
         echo $this->protected;
         echo $this->private;
     }
 }
 
-$obj = new MyClass();
+$obj = new MyClass(); //Instanciamos $obj con la class MyClass()
 echo $obj->public;    // Funciona bien
 //echo $obj->protected; // Error Fatal
 //echo $obj->private;   // Error Fatal
@@ -24,17 +25,20 @@ $obj->printHello();   // Muestra Public, Protected y Private
 
 
 /**
- * Definición de MyClass2
+ * Definición de MyClass2 una "subclase"
  */
 class MyClass2 extends MyClass {
     // Se pueden redeclarar las propiedades pública y protegida, pero no la privada
     public $public = 'Public2';
     protected $protected = 'Protected2';
+    private $otroatributo = "hola"; //Único de MyClass2
+    
 
     function printHello(){
         echo $this->public;
         echo $this->protected;
         //echo $this->private;
+        echo $this->otroatributo;
     }
 }
 
@@ -42,6 +46,7 @@ $obj2 = new MyClass2();
 echo $obj2->public;    // Funciona bien
 //echo $obj2->protected; // Error Fatal
 //echo $obj2->private;   // Undefined
+//echo $obj2->otroatributo;
 $obj2->printHello();   // Muestra Public2, Protected2, Undefined
 
 ?>
