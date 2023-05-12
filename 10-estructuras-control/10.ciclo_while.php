5efef79180236122eaad1d9fb12627ea12ee94b6
Marc Esteve Garcia <41911219+MarcEsteve@users.noreply.github.com>
2023-05-12 16:38:11 +0200

    bucles loops de control for y while y dowhile

--- 10-estructuras-control/10.ciclo_while.php
+++ 10-estructuras-control/10.ciclo_while.php
@@ -2,10 +2,19 @@
 
 $x = 20;
 
-while($x >= 1){
-	echo $x . '<br>';
+// while($x >= 1){
+// 	echo $x . '<br>';
 
-	$x--;
+// 	$x--;
+// }
+
+###EJERCICIO muestra del 15 al 25 incluidos
+
+$i=15;
+
+while($i <= 25){
+	echo $i . '<br>';
+	$i++; // Equivalente $i=$i+1; $i+=1; 
 }
 
 ?>
--- 10-estructuras-control/10.recorriendo_arrays.php
+++ 10-estructuras-control/10.recorriendo_arrays.php
@@ -5,15 +5,21 @@ $meses = array(
 	'Mayo', 'Junio', 'Julio', 'Agosto',
 	'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'
 );
+// echo count($meses); //12
+// $meses[11]; //Diciembre
+// $meses[count($meses)-1];
 
-// echo count($meses);
-
-for($i = 0; $i < count($meses); $i++){
-	echo $meses[$i] . '<br />';
-}
+// for($i = 0; $i < count($meses); $i++){
+// 	echo $meses[$i] . '<br />';
+// }
 
 
 $contador = 0;
+$stockProducto=100;
+
+//Aqui podria cambiar constantemente el $contador
+// $stockProducto se modifica en el tiempo
+
 while($contador < count($meses)){
 	echo $meses[$contador]. '<br />';
 
