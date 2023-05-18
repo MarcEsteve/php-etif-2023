433a2711bf44ec36dfd99fe5ca1f3be3971fc234
Marc Esteve Garcia <41911219+MarcEsteve@users.noreply.github.com>
2023-05-18 16:05:11 +0200

    Create imprimir.php

---
+++ UF1844/imprimir.php
@@ -0,0 +1,18 @@
+<?php
+
+    class Impressora {
+        
+    }
+    
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>Impressió del document</title>
+</head>
+<body>
+    <h1>Titular del document</h1>
+    <!-- Codi PHP -->
+</body>
+</html>
