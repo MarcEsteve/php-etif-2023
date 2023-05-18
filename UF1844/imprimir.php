cf7bf6bdadc071c7b59f7356ee4b359b33faa666
Marc Esteve Garcia <41911219+MarcEsteve@users.noreply.github.com>
2023-05-18 16:47:11 +0200

    Update imprimir.php

--- UF1844/imprimir.php
+++ UF1844/imprimir.php
@@ -1,9 +1,32 @@
 <?php
 
     class Impressora {
-        
+
+        //Atributos
+        public $tinta = true; //Boolean
+        public $paper = true; //Boolean
+        public $tamany = "DINA4"; //String
+        public $color = "negre"; //String
+
+        //Métodos
+        public function imprimeix($tinta, $paper, $tamany, $color) {
+            //Añadir el parámetro $color
+            if ($tinta && $paper) {
+                echo "Es pot imprimir en " . $tamany . ", en color " . $color;
+            } else {
+                echo "Falta tinta o paper";
+            }
+        }
     }
+    // Ejercicio 3B
+    $semana = array("Dilluns","Dimarts","Dimecres","Dijous","Divendres","Dissabte","Diumenge");
+
+    //Ej 4 incial ordenar array $semana
+    sort($semana);
+    //var_dump($semana); //Comprovar que se ha ordenado
+
     
+
 
 ?>
 <!DOCTYPE html>
@@ -13,6 +36,25 @@
 </head>
 <body>
     <h1>Titular del document</h1>
-    <!-- Codi PHP -->
+    
+    <ul>
+        <!-- Codi PHP del ejercicio 4 mostrar lista del array $semana ordenada-->
+        <?php
+            foreach ($semana as $dia) {
+                echo "<li>" . $dia . "</li>";
+            }
+        ?> 
+    </ul>
+    <?php
+    //Ejercicio 5 A
+        //Para mostrar "Es pot imprimir en DINA4, color negre"
+        $hp = new Impressora(); //Instanciación del objeto de la class Impressora
+        // Se utilizan los atributos y los métodos a través del objeto $hp
+        //Con el uso del método
+        $hp->imprimeix(true, true, "DINA4", "negre");
+        echo "<br>";
+        //Con el uso de los atributos por separado y concatenados
+        echo "Es pot imprimir en {$hp->tamany}, en color {$hp->color}";
+    ?>
 </body>
 </html>
