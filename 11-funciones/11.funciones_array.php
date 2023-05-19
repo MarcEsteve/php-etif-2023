353947bdd4a4b6e8c495dd265920ae40bf9b26b2
Marc Esteve Garcia <41911219+MarcEsteve@users.noreply.github.com>
2023-05-19 19:22:17 +0200

    funciones +

--- 11-funciones/11.funciones_array.php
+++ 11-funciones/11.funciones_array.php
@@ -43,17 +43,54 @@ $frutas = array("naranja", "plátano");
 array_push($frutas, "manzana", "arándano");
 // print_r($frutas);
 
-echo join(', ', $semana); //Une los valores de array con un salto de linea
+// echo join(', ', $semana); //Une los valores de array con un salto de linea
 
 ###EJERCICIO
 // Genera una enumeración de $semana como un join() pero que el último valor coloque una conjunción "y" en vez de una coma ","
 
+// OPCIÓN 1 con Funciones Hernán
+// $ultimo_dia = array_pop($semana); //Domingo
+// echo join(', ', $semana);
+// echo ' y ', $ultimo_dia;
+
+// OPCIÓN 2 con Bucles Xavi
+
+// $contar=count($semana); //7
+// echo $contar;
+// for ($i=0; $i<$contar; $i++) {    
+// 	if($i==$contar-1){
+//     	echo "y $semana[$i]";
+// 	} elseif($i==$contar-2){
+//         echo "$semana[$i] ";
+// 	} else {
+//     	echo "$semana[$i], ";
+// 	}
+// }
+
+//OPCIÓN 3 Funciones y array_slice() Joan
+
+// $semana = array('Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo');
+// $ultimo_dia = array_slice($semana, -1); //Domingo
+// $otros_dias = array_slice($semana, 0, -1); 
+// //$semana sin Domingo
+// echo implode(', ', $otros_dias) . ' y ' . $ultimo_dia[0];
+
+
 // echo count($semana); //cuenta cuandos dias hay en el array $semana
 
-// rsort($semana);
+// rsort($semana); //Ordenar sort() o inverso "reverse" rsort()
 // echo join(', ', $semana);
 
 $semana_reverse = array_reverse($semana); //Orden inverso del array
 // echo join(', ', $semana_reverse);
+// echo "<br>";
+// echo implode(', ', $semana);
+
+$finDeSemana = array_slice($semana, -2, count($semana));
+$finDeSemana = array_slice($semana, -2);
+echo implode(', ', $finDeSemana);
+
+###FUNCIONES de Arrays
+// https://www.php.net/manual/es/ref.array.php
 
 ?>
