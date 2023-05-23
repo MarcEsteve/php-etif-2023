0b180bc62241f41904e3008e7a972912f0cbc7ae
Marc Esteve Garcia <41911219+MarcEsteve@users.noreply.github.com>
2023-05-23 17:14:55 +0200

    commit 23 Mayo formularios

--- 12-app-formulario/recibe-get.php
+++ 12-app-formulario/recibe-get.php
@@ -1,14 +1,19 @@
 <?php
 
-// print_r($_GET);
+print_r($_GET);
 
 
 #Muestra en pantalla lo que hemos rellenado del formulario se puede editar y modificar el array del get
 //Incluso inyectar código: <h1>Hola</h1> o <script>alert('Hola')</script>
 
 if(!$_GET) {
-    header('Location: http://localhost/cursoweb/formularios/ejemplo-form-index.php'); //Carpeta y archivo para rellenar el form correcto
+    header('Location: http://localhost/php-etif-2023/12-app-formulario/ejemplo-form-index.php'); //Carpeta y archivo para rellenar el formulario correcto, en recibe-get.php no tendremos datos si el submit no se ha realizado ya que $_GET estará vacio
 }
+
+// $nombre = $_GET['nombre'];
+// $sexo = $_GET['sexo'];
+// $fecha = $_GET['fecha'];
+// $terminos = $_GET['terminos'];
 
 //VALIDACIÓN NOMBRE
 if ($_GET['nombre']) { 
@@ -16,7 +21,8 @@ if ($_GET['nombre']) {
 } else {
     echo 'El usuario no ha escrito su nombre';
 }
-//VALIDACIÓN SEXO
+
+// VALIDACIÓN SEXO
 if ($_GET['sexo']) { 
     $sexo = $_GET['sexo'];
 } else {
@@ -29,17 +35,15 @@ if ($_GET['fecha']) {
 } else {
     echo 'El usuario no ha escrito su fecha';
 }
-if ($_GET['terminos']) { //VALIDACIÓN TERMINOS
+
+//VALIDACIÓN TERMINOS
+if ($_GET['terminos']) { 
     $terminos = $_GET['terminos'];
-}
-else {
+} else {
     echo 'El usuario no ha aceptado terminos';
 }
-// $nombre = $_GET['nombre'];
-// $sexo = $_GET['sexo'];
-// $fecha = $_GET['fecha'];
-// $terminos = $_GET['terminos'];
 
-echo 'Hola ' . $nombre . ' eres un ' . $sexo . ' y has escogido la fecha: '. $fecha;
+
+echo 'Hola ' . $nombre . ' eres un ' . $sexo . ' y has escogido la fecha: '. $fecha . ' y  se han aceptado terminos? ' . $terminos;
 
 ?>
