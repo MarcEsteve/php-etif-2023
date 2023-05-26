e96c51a15e3124c43d582d60c60d82e96ee97d67
Marc Esteve Garcia <41911219+MarcEsteve@users.noreply.github.com>
2023-05-26 17:22:03 +0200

    ejercicio form app php

--- 12-app-formulario/ejercicios/ejercicio-captura.php
+++ 12-app-formulario/ejercicios/ejercicio-captura.php
@@ -1,24 +1,12 @@
 <?php
 
-$errores='';
-$enviado = false;
+  $errores='';
+  $enviado = false;
+  $nombre ='';
+  $correo ='';
+  $telefono ='';
+  $mensaje ='';
 
-require "vista.php";
-
-?>
-
-<!DOCTYPE html>
-<html>
-
-<head>
-  <title>Resultado del formulario</title>
-  <link rel="stylesheet" href="estilo.css">
-</head>
-
-<body>
-  <h1>Resultado del formulario</h1>
-
-  <?php
   function filtrarCorreo($correo) {
     // Filtrar y validar el formato de correo electrónico
     $correo = filter_var($correo, FILTER_SANITIZE_EMAIL);
@@ -28,50 +16,33 @@ require "vista.php";
       return false;
     }
   }
-
-  if ($_SERVER["REQUEST_METHOD"] == "GET") {
-    $nombre = $_GET["nombre"];
-    $correo = $_GET["correo"];
-    $telefono = $_GET["telefono"];
-    $mensaje = $_GET["mensaje"];
-
+  
+  if ($_SERVER["REQUEST_METHOD"] == "GET" ) {
     //Para los errores
-    if (!empty($nombre)) {
+    if (!empty($_GET["nombre"])) {
+      $nombre = $_GET["nombre"];
       $nombre = htmlspecialchars($nombre);
       //Cuando tengo datos, los muestro al final o en el mismo campo si se ha rellenado
     } else {
-      $errores .= '<li>Por favor escribe un nombre</li>';
+      $errores .= '<li class="errores">Por favor escribe un nombre</li>';
     }
-    if (!empty($correo)) {
+    if (!empty($_GET["correo"])) {
+      $correo = $_GET["correo"];
       $correo = filtrarCorreo($correo);
     } else {
-      $errores .= '<li>Por favor escribe un correo</li>';
+      $errores .= '<li class="errores">Por favor escribe un correo</li>';
     }
-    if (!empty($telefono)) {
+    if (!empty($_GET["telefono"])) {
+      $telefono = $_GET["telefono"];
       $telefono = htmlspecialchars($telefono);
     } else {
-      $errores .= '<li>Por favor escribe un teléfono</li>';
+      $errores .= '<li class="errores">Por favor escribe un teléfono</li>';
     }
-    if (!empty($mensaje)) {
+    if (!empty($_GET["mensaje"])) {
+      $mensaje = $_GET["mensaje"];
       $mensaje = htmlspecialchars($mensaje);
     } else {
-      $errores .= '<li>Por favor escribe un mensaje</li>';
-    }
-
-    if (!$errores) {
-      echo "<h2>Datos recibidos:</h2>";
-      echo "<p><strong>Nombre:</strong> " . $nombre . "</p>";
-      echo "<p><strong>Correo:</strong> " . $correo . "</p>";
-      echo "<p><strong>Teléfono:</strong> " . $telefono . "</p>";
-      echo "<p><strong>Mensaje:</strong> " . $mensaje . "</p>";
-    } else {
-      echo "<p>Faltan algunos campos por cumplimentar</p>";
-    }
-    if (isset($_GET['iniciarsesion'])) {
-      echo 'Has iniciado sesión correctamente (por GET) <br>';
-    }
-    if (isset($_GET['registrar'])) {
-      echo 'Te has registrado correctamente (por GET) <br>';
+      $errores .= '<li class="errores">Por favor escribe un mensaje</li>';
     }
   } else {
     $nombre = $_POST["nombre"];
@@ -101,26 +72,33 @@ require "vista.php";
     } else {
       $errores .= '<li>Por favor escribe un mensaje</li>';
     }
+  }
 
-    if (!$errores) {
-      echo "<h2>Datos recibidos:</h2>";
+  require "vista.php";
+
+  echo "<h2>Resultado del formulario</h2>";
+  if ($nombre && $correo && $telefono && $mensaje) {
+      echo "<h3>Datos recibidos:</h3>";
       echo "<p><strong>Nombre:</strong> " . $nombre . "</p>";
       echo "<p><strong>Correo:</strong> " . $correo . "</p>";
       echo "<p><strong>Teléfono:</strong> " . $telefono . "</p>";
       echo "<p><strong>Mensaje:</strong> " . $mensaje . "</p>";
       $enviado=true;
-    } else {
+      if (isset($_GET['iniciarsesion'])) {
+        echo 'Has iniciado sesión correctamente (por GET) <br>';
+      }
+      if (isset($_GET['registrar'])) {
+        echo 'Te has registrado correctamente (por GET) <br>';
+      }
+      if (isset($_POST['iniciarsesion'])) {
+        echo 'Has iniciado sesión correctamente (por POST) <br>';
+      }
+      if (isset($_POST['registrar'])) {
+        echo 'Te has registrado correctamente (por POST) <br>';
+      }
+  } else {
       echo "<p>Faltan algunos campos por cumplimentar</p>";
-    }
-    if (isset($_POST['iniciarsesion'])) {
-      echo 'Has iniciado sesión correctamente (por POST) <br>';
-    }
-    if (isset($_POST['registrar'])) {
-      echo 'Te has registrado correctamente (por POST) <br>';
-    }
   }
-  
-  ?>
-  <!-- <a href="vista.php"><button>Volver al formulario</button></a> -->
-</body>
-</html>
+?>
+
+<a href="ejercicio-captura.php"><button>Rellenar un nuevo formulario</button></a>
--- 12-app-formulario/ejercicios/vista.php
+++ 12-app-formulario/ejercicios/vista.php
@@ -21,22 +21,29 @@
   
   <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="get">
     <label for="nombre">Nombre:</label>
-    <input type="text" id="nombre" name="nombre">
+    <input type="text" id="nombre" name="nombre" value="<?php if(!$enviado && isset($nombre)) echo $nombre ?>">
+
+    <!-- Equivalente a la sintaxis clásica -->
+    <!-- <?php 
+      // if(!$enviado && isset($nombre)) {
+      //   echo $nombre;
+      // }  
+    ?> -->
     
     <br>
     
     <label for="correo">Correo:</label>
-    <input type="email" id="correo" name="correo">
+    <input type="email" id="correo" name="correo" value="<?php if(!$enviado && isset($correo)) echo $correo ?>">
     
     <br>
     
     <label for="telefono">Teléfono:</label>
-    <input type="tel" id="telefono" name="telefono">
+    <input type="tel" id="telefono" name="telefono" value="<?php if(!$enviado && isset($telefono)) echo $telefono ?>">
     
     <br>
     
     <label for="mensaje">Mensaje:</label>
-    <textarea id="mensaje" name="mensaje"></textarea>
+    <textarea id="mensaje" name="mensaje"><?php if(!$enviado && isset($mensaje)) echo $mensaje ?></textarea>
     
     <br>
     
@@ -52,6 +59,8 @@
     
     <input type="submit" name="iniciarsesion" value="Iniciar Sesión">
     <input type="submit" name="registrar" value="Registrar">
+    <br>
+    <br>
   </form>
 </body>
 </html>
