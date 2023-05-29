1cf74c5abbd7a3d8ae15153893367dcc1bb30047
Marc Esteve Garcia <41911219+MarcEsteve@users.noreply.github.com>
2023-05-29 16:48:53 +0200

    Update sesion-id.php

--- 13-sesiones/sesion-id.php
+++ 13-sesiones/sesion-id.php
@@ -7,6 +7,8 @@ if (!isset($_SESSION['SID'])) {
     $_SESSION['SID'] = session_id();
 }
 
+//Función nueva session_id() genera un un id de la sesión para un usuario por ejemplo
+
 // Mostrar el SID
 echo "El SID de la sesión actual es: " . $_SESSION['SID'];
 
