85eed2c3801e5db71aa02e384e1b761c3786c3a4
Marc Esteve Garcia <41911219+MarcEsteve@users.noreply.github.com>
2023-05-29 15:41:37 +0200

    Ejercicio propuesto con usuario en sesion

--- 13-sesiones/ejemplo1.php
+++ 13-sesiones/ejemplo1.php
@@ -13,4 +13,9 @@ echo '<br /><a href="ejemplo2.php">Ejemplo 2</a>';
 
 // O quizás pasar el id de sesión, si fuera necesario
 // echo '<br /><a href="ejemplo2.php?' . SID . '">página 2</a>';
+
+#EJERCICIO SESIÓN:
+// 1. Genera un input de formulario para guardar por la visita en el ejemplo2.php y se lo asigne a la variable $_SESSION['usuario']
+// 2. En ejemplo2.php pide de nuevo el usuario y con un condicional le evalues si es el usuario correcto o no. SI es correcto muestre "usuario correcto", siNO muestre "usuario no existe"
+
 ?>
