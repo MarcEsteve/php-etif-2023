342829077a986cca3a4b8833dee0b2316a953f92
Marc Esteve Garcia <41911219+MarcEsteve@users.noreply.github.com>
2023-07-06 17:25:45 +0200

    Update pdo_id.php

--- MF0493_3/mysql/pdo_id.php
+++ MF0493_3/mysql/pdo_id.php
@@ -1,13 +1,14 @@
 <?php
 
-$id = $_GET['id'];
+// $id = $_GET['id'];
 
 try {
-	$conexion = new PDO('mysql:host=localhost;dbname=ejercicios_sql', 'root', '');
+	$conexion = new PDO('mysql:host=localhost;dbname=prueba_consola', 'root', '');
+	$id = isset($_GET['id']) ? $_GET['id'] : 1;
 	$resultados = $conexion->query("SELECT * FROM usuarios WHERE id = $id");
 
     foreach($resultados as $fila){
-		echo $fila['ID'] . ' - ' . $fila['nombre'] . "<br />";
+		echo $fila['id'] . ' - ' . $fila['nombre'] . "<br />";
 	}
 
 }catch(PDOException $e){
