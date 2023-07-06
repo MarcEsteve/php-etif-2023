166d72b616e25662c45a5d4266d8c3ac3846df32
Marc Esteve Garcia <41911219+MarcEsteve@users.noreply.github.com>
2023-07-06 17:14:51 +0200

    Conexiones MySQL a través de PDO más ejercicio

--- MF0493_3/mysql/mysqli_leer-datos.php
+++ MF0493_3/mysql/mysqli_leer-datos.php
@@ -1,29 +1,34 @@
 <?php
 // Realizamos una conexion a la base de datos.
-$conexion = new mysqli('localhost', 'root', '', 'prueba_datos');
+$conexion = new mysqli('localhost', 'root', '', 'prueba_consola');
 
 // Nos retorna un codigo de error o si todo esta bien regresa 0.
-// echo $conexion->connect_errno;
+//echo $conexion->connect_errno;
+
+//echo "Conexión correcta <br>";
 
 if ($conexion->connect_errno){
 	die('Lo siento hubo un problema con el servidor');
 } else {
-	// $id = isset($_GET['id']) ? $_GET['id'] : 1;
-	// $sql = "SELECT * FROM usuarios WHERE ID = $id";
-	$sql = "SELECT * FROM usuarios";
+	$id = isset($_GET['id']) ? $_GET['id'] : 1;
+	$sql = "SELECT * FROM usuarios WHERE id = $id";
+	// $sql = "SELECT * FROM usuarios WHERE id = 3";
+	// $sql = "SELECT * FROM usuarios";
 	$resultado = $conexion->query($sql);
 	
 	if($resultado->num_rows){
 		// echo '<pre>';
-		// var_dump($resultado->fetch_assoc());
-		// echo $resultado->fetch_assoc()['nombre'];
+		//var_dump($resultado->fetch_assoc()); //id 1
+		//var_dump($resultado->fetch_assoc()); //id 2, etc
+		//echo $resultado->fetch_assoc()['nombre']; //Primer nombre
+		// echo $resultado->fetch_assoc()['nombre']; //Segundo nombre
 		// echo '</pre>';
 
 		while($fila = $resultado->fetch_assoc()){
-			echo $fila['ID'] . ' - ' . $fila['nombre'] . '<br />';
+			echo $fila['id'] . ' - ' . $fila['nombre'] . '<br />';
 		}
 
 	} else {
-		echo 'No hay datos';
+		echo 'No hay datos, no hay registros';
 	}
 }
--- MF0493_3/mysql/pdo.php
+++ MF0493_3/mysql/pdo.php
@@ -1,11 +1,11 @@
 <?php 
 try {
 	
-	$conexion = new PDO('mysql:host=localhost;dbname=prueba_datos', 'root', '');
+	$conexion = new PDO('mysql:host=localhost;dbname=prueba_consola', 'root', '');
 	// echo "Conexion correcta";
 
 	// Metodo Query (metodo desaconsejado)
-	$resultados = $conexion->query("SELECT * FROM usuarios WHERE id = 2");
+	$resultados = $conexion->query("SELECT * FROM usuarios WHERE id=2 "); //Objeto array asociativo que guarda todo el registro 2/pepito/correo
 
 	foreach($resultados as $fila){
 		echo $fila['nombre'] . "<br />";
