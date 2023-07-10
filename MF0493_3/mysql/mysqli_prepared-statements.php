95127d8d89ef8849e12625c3a4f6e24f3c0d0b1d
Marc Esteve Garcia <41911219+MarcEsteve@users.noreply.github.com>
2023-07-10 15:48:25 +0200

    Update mysqli_prepared-statements.php

--- MF0493_3/mysql/mysqli_prepared-statements.php
+++ MF0493_3/mysql/mysqli_prepared-statements.php
@@ -1,22 +1,23 @@
 <?php
-$conexion = new mysqli('localhost', 'root', '', 'prueba_datos');
+$conexion = new mysqli('localhost', 'root', '', 'prueba_consola');
 
 if ($conexion->connect_errno){
 	die('Lo siento hubo un problema con el servidor');
 } else {
-	$statement = $conexion->prepare("INSERT INTO usuarios(id, nombre, edad) VALUES(?, ?, ?)");
+	$statement = $conexion->prepare("INSERT INTO usuarios(id, nombre, email, edad) VALUES(?, ?, ?, ?)");
 	
 	// Remplazamos los placeholder ? con los valores que queremos usar.
 		// Una S por placeholder que tengamos.
 		// s = string
 		// i = integer
 		// d = double
-	$statement->bind_param('ssi', $id, $nombre, $edad);
+	$statement->bind_param('sssi', $id, $nombre, $email, $edad);
 	$id = NULL;
 
 	// Comprobamos que hayamos pasado un nombre por URL
-	if(isset($_GET['nombre']) && isset($_GET['edad'])){
+	if(isset($_GET['nombre']) && isset($_GET['email']) && isset($_GET['edad'])){
 		$nombre = $_GET['nombre'];
+		$email = $_GET['email'];
 		$edad = $_GET['edad'];
 	}
 
